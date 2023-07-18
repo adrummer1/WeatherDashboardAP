9283c11d81f7415eae1dbec7a0499f63bbbd6521
adrummer1 <andrew@bluestemcreativeco.com>
2023-07-18 12:34:14 -0500

    Added divs and textContent/appends for current weather.

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var citySearchTerm = document.querySelector("#weather-search")
 var APIKey = "c1e47e9d5eb11ce67e07485421770f51";
 var apiBase = "api.openweathermap.org/data/2.5/";
 var units = "imperial";
-var currentDate = dayjs().format("dddd MMM, YYYY")
+var currentDate = dayjs().format("dddd, MMM DD, YYYY")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -23,7 +23,7 @@ var formSubmitHandler = function(event) {
 };
 
 function getUserCity(city) {
-var currentWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var currentWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=" + units;
 
 fetch(currentWeather)
     .then(function (response) {
@@ -42,25 +42,42 @@ fetch(currentWeather)
 var displayCity = function (city, searchTerm) {
     if (city.length === 0) {
         weatherNowEl.textContent = "City not found.";
-    } return;
-}
+    return;
+    }
+
+    var currentWeatherHTML = document.getElementsByClassName("cityInfo")[0];
+    currentWeatherHTML.append(city.name);
+    var weatherDate = document.createElement("div");
+    weatherDate.textContent = currentDate;
+    var temp = document.createElement("div");
+    temp.textContent = "Temperature: " + city.main.temp;
+    var humidity = document.createElement("div");
+    humidity.textContent = "Humidity: " + city.main.humidity;
+    var wind = document.createElement("div");
+    wind.textContent = "Wind Speed: " + city.wind.speed;
+    currentWeatherHTML.append(weatherDate);
+    currentWeatherHTML.append(city.weather[0].icon);
+    currentWeatherHTML.append(temp);
+    currentWeatherHTML.append(humidity);
+    currentWeatherHTML.append(wind);
+};
 
 for (var i = 0; i < city.length; i++) {
     var cityName = city[i].city.name;
 
-    var cityEl = document.createElement('p');
-    cityEl.classList = "list-item flex-row justify-space-between align-center";
-    cityEl.textContent = cityName;
+    // var cityEl = document.createElement('p');
+    // cityEl.classList = "list-item flex-row justify-space-between align-center";
+    // cityEl.textContent = cityName;
 
     // var headerEl = document.createElement("span");
     // headerEl.textContent = cityName;
 
     // cityEl.appendChild(headerEl);
     
-    weatherNowEl.appendChild(cityEl);
+    // weatherNowEl.appendChild(cityEl);
 }
 
-weatherNowEl.textContent = cityName;
+// weatherNowEl.textContent = cityName;
 
 
 
